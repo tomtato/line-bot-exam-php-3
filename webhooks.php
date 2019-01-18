b51003ef9989dd79c87edf15c2127094f8e64d93
tomtato <satang_1982@hotmail.com>
2019-01-19 01:34:21 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token ='8jwuHkkOKAM0eouMDh8T9w0NPsDabh0cud/3jOJTDzw/LonPQFIoAmJfyH9MomPmXVRdm1jJajkgrervc8/pN8hhy0eziVvVuZWmE8CmZlmG6UskBb1Ky6ZmWLXp7OgvYaBNtZpb1csFBzZyUDqLtAdB04t89/1O/w1cDnyilFU=';
+$access_token ='Mdw5+0PXg2SddX7CUl/VafbJ21DfguIFUw645n8zpnQ32mWZOxCM9Q1nGUlMYP5dXVRdm1jJajkgrervc8/pN8hhy0eziVvVuZWmE8CmZlnAasiOQQbFbEvdEOb/hCS73A3oNrxOdC9chF0KV92jnwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
